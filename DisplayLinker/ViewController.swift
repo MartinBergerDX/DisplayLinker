e3173d01e7ccc3a7c91292914d588a22b403da08
Martin Berger <martin.berger@codecentric.de>
2016-10-12 11:30:50 +0200

    Implemented

---
+++ DisplayLinker/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DisplayLinker
+//
+//  Created by Martin Berger on 10/12/16.
+//  Copyright © 2016 heavy-debugging.inc All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, DisplayLinkerDelegate {
+    var linker: DisplayLinker? = nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.linker = DisplayLinker.init(withDelegate: self)
+    }
+
+    func displayWillUpdate(delta: TimeInterval) {
+        print(String(format: "%2.4f", delta))
+    }
+}
+
